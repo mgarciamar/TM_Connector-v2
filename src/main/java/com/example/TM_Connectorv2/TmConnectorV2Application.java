92180f960ef4129dae9e9e830a7d4ad5bd9cf862
Monica Garcia <matt@gft.com>
2022-11-04 13:54:42 +0100

    Initial commit

---
+++ src/main/java/com/example/TM_Connectorv2/TmConnectorV2Application.java
@@ -0,0 +1,13 @@
+package com.example.TM_Connectorv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmConnectorV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmConnectorV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TM_Connectorv2/TmConnectorV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TM_Connectorv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmConnectorV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
